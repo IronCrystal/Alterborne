03ba40ee306064b0698cbcac9d58ea19a9738842
OakQ <oquinter@gmu.edu>
2016-07-20 22:45:27 -0400

    Cone Attack Added
    Maybe for swords

--- Assets/MY STUFF/Scripts/DamageEnemy.cs
+++ Assets/MY STUFF/Scripts/DamageEnemy.cs
@@ -2,19 +2,68 @@
 using System.Collections;
 
 public class DamageEnemy : MonoBehaviour {
-    private int countSinceDamageTaken = 0;
-    public int timeBetweenDamage;
+    
+    public float cooldown;
+    float cd;
+    RaycastHit2D hit;
+
+    int layerPlayer = 8;
+    int raycastLayer;
+
+    Quaternion startingAngle = Quaternion.AngleAxis(-60, Vector3.forward);
+    Quaternion stepAngle = Quaternion.AngleAxis(24, Vector3.forward);
+
     // Use this for initialization
     void Start () {
-	
+        cd = cooldown;
+        cooldown = 0;
+        layerPlayer = 1 << layerPlayer;
+        raycastLayer = ~layerPlayer;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        cooldown -= Time.deltaTime;
 	}
 
-    void OnCollisionEnter2D(Collision2D other) {
+    public void attackCone()
+    {
+        if (cooldown <= 0)
+        {            
+            Quaternion angle = transform.rotation * startingAngle;
+            Vector3 direction = angle * Vector3.right;
+            Vector3 pos = transform.position;
+            for (int i = 0; i < 5; i++)
+            {
+                Debug.DrawRay(pos, direction, Color.red, 3.0f);
+                hit = Physics2D.Raycast(pos, direction, 1.0f, raycastLayer);
+
+                if (hit.collider != null){
+                    //Debug.Log(hit.collider.name);
+                    if (hit.collider.tag == "Enemy")
+                    {
+                        GameObject thePlayer = GameObject.Find("player");
+                        Stats playerStats = thePlayer.GetComponent<Stats>();
+                        float strength = playerStats.strength;
+                        Stats enemyStats = hit.collider.gameObject.GetComponent<Stats>();
+                        float armor = enemyStats.armor;
+
+                        int damage = Mathf.RoundToInt(strength - armor);
+                        if (damage <= 0)
+                            damage = 1;
+                        enemyStats.currentHP -= damage;
+                        //Debug.Log(damage);
+                        break;
+                    }
+                }
+                direction = stepAngle * direction;
+            }          
+            cooldown = cd;
+        }
+
+    }
+
+    /*void OnCollisionEnter2D(Collision2D other) {
         if (other.gameObject.tag == "Enemy") {
             GameObject thePlayer = GameObject.Find("player");
             Stats playerStats = thePlayer.GetComponent<Stats>();
@@ -22,9 +71,9 @@ public class DamageEnemy : MonoBehaviour {
             playerStats.currentHP--;
             enemyStats.currentHP--;
         }
-    }
+    }*/
 
-    void OnCollisionStay2D(Collision2D other) {
+    /*void OnCollisionStay2D(Collision2D other) {
         if (other.gameObject.tag == "Enemy") {
             if (countSinceDamageTaken >= timeBetweenDamage) {
                 GameObject thePlayer = GameObject.Find("player");
@@ -41,5 +90,5 @@ public class DamageEnemy : MonoBehaviour {
 
     void OnCollisionExit2D(Collision2D other) {
         countSinceDamageTaken = 0;
-    }
+    }*/
 }
