abe7b728953357ad36e48723585648c7029188cf
IronCrystal <apeace95@gmail.com>
2016-07-24 00:32:47 -0400

    Added a login screen.  Works!

---
+++ Assets/MY STUFF/Scripts/Network/VerifyLogin.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class VerifyLogin : MonoBehaviour {
+
+    public InputField inputUsername;
+    public InputField inputPassword;
+
+    void Start() {
+        
+    }
+
+    IEnumerator WaitForRequest(WWW www) {
+        yield return www;
+
+         // check for errors
+        if (www.error == null) {
+            Debug.Log(www.data);
+        } else {
+            Debug.Log(www.error);
+        }
+    }
+
+    public void Login() {
+        string username = inputUsername.text;
+        string password = inputPassword.text;
+        string url = "localhost/test.php";
+
+        WWWForm form = new WWWForm();
+        form.AddField("username", username);
+        form.AddField("password", password);
+        WWW www = new WWW(url, form);
+
+        StartCoroutine(WaitForRequest(www));
+    }
+}
